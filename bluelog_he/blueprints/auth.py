626219e29458c3484d3e1dc71ab614d89a83b14e
henry199101 <331243543@qq.com>
2019-01-06 14:01:51 +0800

    Zhuang Pei blueprint: add view functions [login & logout] .

---
+++ bluelog_he/blueprints/auth.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+from flask import Flask, Blueprint
+
+auth_bp = Blueprint('auth', __name__)
+
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    return 'The login page'
+
+
+@auth_bp.route('/logout')
+def logout():
+    return 'The logout page'
