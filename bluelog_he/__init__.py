37d5b971b508ad45fc78cc2cbc5aad5b86437e7b
henry199101 <331243543@qq.com>
2019-01-06 14:14:43 +0800

    Main change: register blueprint [auth_bp] .

--- bluelog_he/__init__.py
+++ bluelog_he/__init__.py
@@ -16,9 +16,9 @@ from flask import Flask
 
 
 
-
-from bluelog_he.blueprints.blog import blog_bp
+from bluelog_he.blueprints.admin import admin_bp
 from bluelog_he.blueprints.auth import auth_bp
+from bluelog_he.blueprints.blog import blog_bp
 
 
 app = Flask('bluelog_he')
@@ -29,6 +29,6 @@ app = Flask('bluelog_he')
 
 
 
-
 app.register_blueprint(blog_bp)
+app.register_blueprint(admin_bp)
 app.register_blueprint(auth_bp)
