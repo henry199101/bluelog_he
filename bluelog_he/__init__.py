12fbdf23fbb7e7fab9d8e6eeafdd0ebbafebf4ec
henry199101 <331243543@qq.com>
2019-01-06 15:57:53 +0800

    Put registering blueprint into one function .

--- bluelog_he/__init__.py
+++ bluelog_he/__init__.py
@@ -5,30 +5,50 @@
 
 
 
-
+import os
 
 
 from flask import Flask
-
-
-
-
-
-
 
 from bluelog_he.blueprints.admin import admin_bp
 from bluelog_he.blueprints.auth import auth_bp
 from bluelog_he.blueprints.blog import blog_bp
 
-
-app = Flask('bluelog_he')
-
+from bluelog_he.settings import config
 
 
 
 
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG', 'development')
+
+    app = Flask('bluelog_he')
+    app.config.from_object(config[config_name])
 
 
-app.register_blueprint(blog_bp)
-app.register_blueprint(admin_bp)
-app.register_blueprint(auth_bp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def register_blueprints(app):
+    app.register_blueprint(blog_bp)
+    app.register_blueprint(admin_bp, url_prefix='/admin')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
