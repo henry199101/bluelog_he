1ffd8d0c9e73f6f9b7dde11c381364a32a58c719
henry199101 <331243543@qq.com>
2019-01-06 23:15:27 +0800

    Define a function: to generate virtual reviewed/unreviewed/admin/replies comments .

--- bluelog_he/fakes.py
+++ bluelog_he/fakes.py
@@ -11,7 +11,7 @@ from faker import Faker
 from sqlalchemy.exc import IntegrityError
 
 from bluelog_he.extensions import db
-from bluelog_he.models import Admin, Category
+from bluelog_he.models import Admin, Category, Post, Comment
 
 fake = Faker()
 
@@ -51,4 +51,61 @@ def fake_posts(count=50):
         )
 
         db.session.add(post)
+    db.session.commit()
+
+
+def fake_comments(count=500):
+    for i in range(count):
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            post=Post.query.get(randint(1, Post.query()))
+        )
+        db.session.add(comment)
+
+    salt = int(count * 0.1)
+    for i in range(salt):
+        # unreviewed comments
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=False,  # unreviewed comments
+            post=Post.query.get(randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+
+        # comments from admin
+        comment = Comment(
+            author='Mima Kirigoe',
+            email='mima@example.com',
+            site='example.com',
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            from_admin=True,
+            reviewed=True,
+            post=Post.query.get(randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
+    db.session.commit()
+
+    # replies
+    for i in range(salt):
+        comment = Comment(
+            author=fake.name(),
+            email=fake.email(),
+            site=fake.url(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year(),
+            reviewed=True,
+            replied=Comment.query.get(randint(1, Comment.query.count())),
+            post=Post.query.get(randint(1, Post.query.count()))
+        )
+        db.session.add(comment)
     db.session.commit()
