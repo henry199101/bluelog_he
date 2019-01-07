475e63b67c0faa48ab453bdd518ca2cd34ad0c5f
henry199101 <331243543@qq.com>
2019-01-07 20:47:23 +0800

    Add view function: [index] .

--- bluelog_he/blueprints/blog.py
+++ bluelog_he/blueprints/blog.py
@@ -5,9 +5,14 @@
 
 
 
-from flask import Blueprint
+from flask import render_template, Blueprint
 
 blog_bp = Blueprint('blog', __name__)
+
+
+@blog_bp.route('/')
+def index():
+    return render_template('blog/index.html')
 
 
 @blog_bp.route('/about')
