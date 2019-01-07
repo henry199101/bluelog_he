19355bf222e462522f903d8fc7041d665cce5b6a
henry199101 <331243543@qq.com>
2019-01-07 18:00:35 +0800

    Add [CategoryForm]: articles' classifications .

--- bluelog_he/forms.py
+++ bluelog_he/forms.py
@@ -7,7 +7,8 @@
 
 from flask_ckeditor import CKEditorField
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, BooleanField, PasswordField
+from wtforms import StringField, SubmitField, SelectField, ValidationError, BooleanField, PasswordField, \
+
 
 from wtforms.validators import DataRequired
 
@@ -28,7 +29,6 @@ class LoginForm(FlaskForm):
 
 
 
-
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(1, 60)])
     category = SelectField('Category', coerce=int, default=1)
@@ -39,3 +39,12 @@ class PostForm(FlaskForm):
         super(PostForm, self).__init__(*args, **kwargs)
         self.category.choices = [(category.id, category.name)
                                  for category in Category.query.order_by(Category.name).all()]
+
+
+class CategoryForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
+    submit = SubmitField()
+
+    def validate_name(self, field):
+        if Category.query.filter_by(name=field.data).first():
+            raise ValidationError('Name already in use.')
