9f79f38a539cd7784ae68b1ecfaa50f463882199
henry199101 <331243543@qq.com>
2019-01-07 14:45:29 +0800

    Fix a simple bug .

--- bluelog_he/models.py
+++ bluelog_he/models.py
@@ -35,10 +35,10 @@ class Category(db.Model):
 
 
 
-class Post(db.model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
-    body = db.Column(db.text)
+    body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 
